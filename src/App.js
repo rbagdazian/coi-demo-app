dc971ca8a809748a2cc698f68df130a450675993
EC2 Default User <ec2-user@ip-172-31-54-255.ec2.internal>
2021-08-31 16:11:47 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World from React!
+          <br />
+          This is pretty cool!
         </a>
       </header>
     </div>
