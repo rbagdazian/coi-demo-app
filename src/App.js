5a22962e2512035dc350078c6e85fabcdf52ca30
rbagdazian <rbagdazian@radiancetech.com>
2021-08-31 16:59:28 +0000

    added user authentication:

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Hello World from React!
           <br />
           This is pretty cool!
@@ -22,10 +16,13 @@ function App() {
           Git Hub is now connected!
           <br />
           Ci/Cd is working!
-        </a>
+          <br />
+          Authentication is now working!
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
